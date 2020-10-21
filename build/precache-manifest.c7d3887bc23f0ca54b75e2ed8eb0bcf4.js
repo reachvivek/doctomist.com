9ba5c5b529af4f0a0e048a399d746a6bd67f25f5
Vivek Kumar Singh <rogerthatvivek@gmail.com>
2020-10-21 16:12:39 +0530

    Initial Commit

---
+++ build/precache-manifest.c7d3887bc23f0ca54b75e2ed8eb0bcf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f354623f2a8567326c8304de32f837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcd1a490db6078da7844",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b86cb143188bb4e0234",
+    "url": "/static/js/2.dc6b1b67.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dc6b1b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd1a490db6078da7844",
+    "url": "/static/js/main.119ec8a5.chunk.js"
+  },
+  {
+    "revision": "76fc49e556ee63dae1e6",
+    "url": "/static/js/runtime-main.5adde44c.js"
+  },
+  {
+    "revision": "3bd70bc671925f9a8f0dcc55d95a12a3",
+    "url": "/static/media/logo.3bd70bc6.png"
+  }
+]);
